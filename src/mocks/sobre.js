0e4147ff80348ca42ed5142bdb3378f9daca5777
1gorlopes <145722276+1gorlopes@users.noreply.github.com>
2024-05-27 22:11:02 -0300

    Add files via upload

---
+++ src/mocks/sobre.js
@@ -0,0 +1,13 @@
+import Logo from '../../assets/logoblack.png';
+import Img_Producao from '../../assets/jr.png';
+
+const sobre = {
+    textos: {
+        logo: Logo,
+        img_producao: Img_Producao,
+        historia: "No coração da cena urbana, nasceu a Urban WRLD, uma loja que surgiu da paixão pela moda urbana. Criado como um espaço digital para os entusiastas desse estilo inspirada no artista Juice WRLD cativou clientes em todo o país com sua seleção cuidadosa de peças exclusivas e atendimento personalizado. Explore o mundo da moda urbana com facilidade e estilo, tudo a apenas um toque de distância.",
+        texto_imagem: "- One thing my heart tells me is, Flex on a h0e every time they're insecure... 🥶"
+    } 
+}
+
+export default sobre;
